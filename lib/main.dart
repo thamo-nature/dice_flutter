8e052db7a8713ab494c6a2116dc021e4b18b6a94
thamothiran <thamothiran.s@spovum.com>
2024-02-10 00:26:53 +0530

    [develope] - Dice app completed.

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,73 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(const DicePage());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class DicePage extends StatefulWidget {
+  const DicePage({super.key});
+
+  @override
+  State<DicePage> createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftDiceNumber = 1;
+  int rightDiceNumber = 1;
+
+  void diceState() {
+    leftDiceNumber = Random().nextInt(6) + 1;
+    rightDiceNumber = Random().nextInt(6) + 1;
+  }
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
+        backgroundColor: Colors.blue,
+        appBar: AppBar(
+          title: const Center(child: Text('DICE')),
+        ),
         body: Center(
-          child: Text('Hello World!'),
+          child: Row(
+            //crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Expanded(
+                child: TextButton(
+                  onPressed: () {
+                    setState(() {
+                      //print('$leftDiceNumber');
+                      diceState();
+                    });
+                  },
+                  child: Image(
+                    //color: Colors.red,
+                    image: AssetImage('../images/dice$leftDiceNumber.png'),
+                  ),
+                ),
+              ),
+              // SizedBox(
+              //   width: 16,
+              // ),
+              Expanded(
+                child: TextButton(
+                  onPressed: () {
+                    setState(() {
+                      diceState();
+                    });
+
+                    //print("Right");
+                  },
+                  child: Image(
+                    //color: Colors.green,
+                    image: AssetImage('../images/dice$rightDiceNumber.png'),
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
